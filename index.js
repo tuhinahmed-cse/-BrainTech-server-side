8cea626d5e54e71a50cbd76daf01ed9235f5423e
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-10-25 14:13:11 +0600

    individual category data load done

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
-app.get('/', (req, res)=>{
-    res.send('New Hello')
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+        const category_course = courses.find(c => c.category_id === id);
+        res.send(category_course);
 })
 app.listen(Port, ()=>{
     console.log('server Running', Port);
